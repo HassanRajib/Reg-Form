058468eeab48e00844fdad62b2cd4a2325fe74b2
badboy <hasan.rajib39@gmail.com>
2024-04-30 22:27:02 +0600

    Edit to sign up page

--- app/src/main/java/com/example/reg/MainActivity.kt
+++ app/src/main/java/com/example/reg/MainActivity.kt
@@ -1,46 +1,40 @@
 package com.example.reg
 
+
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var name: EditText
-    lateinit var mail: EditText
-    lateinit var pass: EditText
-    lateinit var conPa: EditText
-    lateinit var btn: Button
-    lateinit var jn: EditText
+    lateinit var usNam:EditText
+    lateinit var mAil:EditText
+    lateinit var go: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        name = findViewById(R.id.nameEt)
-        mail = findViewById(R.id.mailEt)
-        pass = findViewById(R.id.passEt)
-        conPa = findViewById(R.id.conPass)
-        btn = findViewById(R.id.btnEt)
-        jgkitn = findViewById(R.id.inEt)
 
-        btn.setOnClickListener {
-            val name = name.text.toString()
-            val mail = mail.text.toString()
-            val pass = pass.text.toString()
-            val conPa = conPa.text.toString()
+        usNam = findViewById(R.id.useNam)
+        mAil = findViewById(R.id.maIl)
+        go = findViewById(R.id.btnAt)
 
-            if (name.isEmpty()||mail.isEmpty()||pass.isEmpty()||conPa.isEmpty()){
-                Toast.makeText(this, "please fill up all the filds", Toast.LENGTH_SHORT).show()
-            }else if (pass!=conPa){
-                Toast.makeText(this, "password dose not match", Toast.LENGTH_SHORT).show()
+
+        go.setOnClickListener {
+        val username = usNam.text.toString()
+        val Email = mAil.text.toString()
+
+            if (username.isEmpty()||Email.isEmpty()){
+                Toast.makeText(this, "please fill up",Toast.LENGTH_SHORT).show()
             }else{
-                Toast.makeText(this, "signup succesful", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "thanks", Toast.LENGTH_SHORT).show()
             }
+
         }
+
     }
 }
