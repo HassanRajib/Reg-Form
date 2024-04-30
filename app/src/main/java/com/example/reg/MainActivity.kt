4d2cf511710b3db8564ad9f9f53b464b0dc5dcca
badboy <hasan.rajib39@gmail.com>
2024-04-30 17:06:42 +0600

    backend added

--- app/src/main/java/com/example/reg/MainActivity.kt
+++ app/src/main/java/com/example/reg/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.example.reg
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var name: EditText
+    lateinit var mail: EditText
+    lateinit var pass: EditText
+    lateinit var conPa: EditText
+    lateinit var btn: Button
+    lateinit var jn: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        name = findViewById(R.id.nameEt)
+        mail = findViewById(R.id.mailEt)
+        pass = findViewById(R.id.passEt)
+        conPa = findViewById(R.id.conPass)
+        btn = findViewById(R.id.btnEt)
+        jgkitn = findViewById(R.id.inEt)
+
+        btn.setOnClickListener {
+            val name = name.text.toString()
+            val mail = mail.text.toString()
+            val pass = pass.text.toString()
+            val conPa = conPa.text.toString()
+
+            if (name.isEmpty()||mail.isEmpty()||pass.isEmpty()||conPa.isEmpty()){
+                Toast.makeText(this, "please fill up all the filds", Toast.LENGTH_SHORT).show()
+            }else if (pass!=conPa){
+                Toast.makeText(this, "password dose not match", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "signup succesful", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
